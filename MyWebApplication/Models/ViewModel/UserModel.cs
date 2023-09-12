ea3d82ad41a833a9494d1f86662448e50d416307
Sayaka Hernandez <sayakaaliyah02@gmail.com>
2023-09-12 22:14:21 +0800

    Second Commit

---
+++ MyWebApplication/Models/ViewModel/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MyWebApplication.Models.ViewModel
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Created By")]
+        public int CreatedBy { get; set; }
+    }
+}
